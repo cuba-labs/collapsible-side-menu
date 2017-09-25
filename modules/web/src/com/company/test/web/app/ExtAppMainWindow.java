17d51441b608adc28db4a327f584f3932cfabdf1
Gleb Gorelov <gorelov@haulmont.com>
2017-09-25 16:53:19 +0400

    Add split

--- modules/web/src/com/company/test/web/app/ExtAppMainWindow.java
+++ modules/web/src/com/company/test/web/app/ExtAppMainWindow.java
@@ -2,7 +2,9 @@ package com.company.test.web.app;
 
 import com.haulmont.cuba.gui.components.AbstractMainWindow;
 import com.haulmont.cuba.gui.components.Embedded;
+import com.haulmont.cuba.gui.components.SplitPanel;
 import com.haulmont.cuba.gui.components.mainwindow.FtsField;
+import com.haulmont.cuba.web.toolkit.ui.CubaHorizontalSplitPanel;
 
 import javax.inject.Inject;
 import java.util.Map;
@@ -15,6 +17,9 @@ public class ExtAppMainWindow extends AbstractMainWindow {
     @Inject
     private Embedded logoImage;
 
+    @Inject
+    private SplitPanel splitPanel;
+
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
@@ -22,5 +27,7 @@ public class ExtAppMainWindow extends AbstractMainWindow {
         initLayoutAnalyzerContextMenu(logoImage);
         initLogoImage(logoImage);
         initFtsField(ftsField);
+
+        splitPanel.unwrap(CubaHorizontalSplitPanel.class).setDockable(true);
     }
 }
